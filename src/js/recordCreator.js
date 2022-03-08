0679add469ea1061de410d4522153395415d00eb
Fredrik Kappmeier <fredrik.kappmeier@live.de>
2022-03-08 14:35:11 +0100

    Restructuring - button for cancelling the process of creating a new data record

--- src/js/recordCreator.js
+++ src/js/recordCreator.js
@@ -39,21 +39,6 @@ function createTextInputLineDiv(name) {
   return lineDiv;
 }
 
-// Loop through all column names to create html elements
-function createTextInputDiv() {
-  const textInputDiv = document.createElement('div');
-  textInputDiv.id = 'text-input-div';
-
-  columnNames.forEach((entry) => {
-    const lineDiv = createTextInputLineDiv(entry);
-    textInputDiv.appendChild(lineDiv);
-  });
-
-  textInputDiv.appendChild(document.createElement('br'));
-
-  document.getElementById('create-record-div').appendChild(textInputDiv);
-}
-
 // simple DOM getter function to create an array containing all text input elements
 function getTextInputs() {
   const array = [];
@@ -65,58 +50,112 @@ function getTextInputs() {
   return array;
 }
 
-// Main function
-export default function createRecord() {
-  const createRecordDiv = document.getElementById('create-record-div');
+// Create new Record object, clear all input values and hide textInputDiv
+function createOnClick() {
   const createRecordButton = document.getElementById('create-record-button');
-  createRecordButton.disabled = true;
-
-  createRecordDiv.appendChild(document.createElement('br'));
-  createRecordDiv.appendChild(document.createElement('br'));
-
-  if (!document.getElementById('text-input-div')) {
-    createTextInputDiv();
-  }
+  const textInputDiv = document.getElementById('text-input-div');
 
   const textInputsArray = getTextInputs();
 
-  const createButton = document.createElement('button');
-  createButton.innerHTML = 'Create';
-  createButton.addEventListener('click', () => {
-    const recordObject = new Record(
-      textInputsArray[0].value,
-      textInputsArray[1].value,
-      textInputsArray[2].value,
-      textInputsArray[3].value,
+  const recordObject = new Record(
+    textInputsArray[0].value,
+    textInputsArray[1].value,
+    textInputsArray[2].value,
+    textInputsArray[3].value,
 
-      textInputsArray[4].value,
-      textInputsArray[5].value,
-      textInputsArray[6].value,
-      textInputsArray[7].value,
+    textInputsArray[4].value,
+    textInputsArray[5].value,
+    textInputsArray[6].value,
+    textInputsArray[7].value,
 
-      textInputsArray[8].value,
-      textInputsArray[9].value,
-      textInputsArray[10].value,
-      textInputsArray[11].value,
+    textInputsArray[8].value,
+    textInputsArray[9].value,
+    textInputsArray[10].value,
+    textInputsArray[11].value,
 
-      textInputsArray[12].value,
-      textInputsArray[13].value,
-      textInputsArray[14].value,
-      textInputsArray[15].value,
-    );
+    textInputsArray[12].value,
+    textInputsArray[13].value,
+    textInputsArray[14].value,
+    textInputsArray[15].value,
+  );
 
-    console.log(recordObject);
+  console.log(recordObject);
 
-    // TODO:
-    // - clearing all values from the text inputs
-    // - hiding the createRecordDiv
-
-    // - adding the information from the new record Object to the table
-    // - creating an array of record objects from the data array (buffer database)
-    // - adding the new record object to the array of already existing records
-
-    // createRecordButton.disabled = false;
+  textInputsArray.forEach((i) => {
+    const input = i;
+    input.value = '';
   });
 
-  createRecordDiv.appendChild(createButton);
+  textInputDiv.style.display = 'none';
+  createRecordButton.disabled = false;
+
+  // TODO:
+  // - clearing all values from the text inputs
+  // - hiding the textInputDiv
+
+  // - adding the information from the new record Object to the table
+  // - creating an array of record objects from the data array (buffer database)
+  // - adding the new record object to the array of already existing records
+}
+
+// Clear all input values and hide textInputDiv
+function cancelOnClick() {
+  const createRecordButton = document.getElementById('create-record-button');
+  const textInputDiv = document.getElementById('text-input-div');
+
+  const textInputsArray = getTextInputs();
+
+  textInputsArray.forEach((i) => {
+    const input = i;
+    input.value = '';
+  });
+
+  textInputDiv.style.display = 'none';
+  createRecordButton.disabled = false;
+}
+
+// Create div with text inputs
+function createTextInputDiv() {
+  const textInputDiv = document.createElement('div');
+  textInputDiv.id = 'text-input-div';
+
+  const headline = document.createElement('h2');
+  const textNode = document.createTextNode('Create a new data record');
+  headline.appendChild(textNode);
+  textInputDiv.appendChild(headline);
+
+  columnNames.forEach((entry) => {
+    const lineDiv = createTextInputLineDiv(entry);
+    textInputDiv.appendChild(lineDiv);
+  });
+
+  textInputDiv.appendChild(document.createElement('br'));
+
+  const createButton = document.createElement('button');
+  createButton.innerHTML = 'Create';
+  createButton.addEventListener('click', createOnClick);
+
+  const cancelButton = document.createElement('button');
+  cancelButton.innerHTML = 'Cancel';
+  cancelButton.addEventListener('click', cancelOnClick);
+
+  textInputDiv.appendChild(createButton);
+  textInputDiv.appendChild(cancelButton);
+
+  document.getElementById('create-record-div').appendChild(textInputDiv);
+}
+
+// Main function
+export default function createRecord() {
+  const createRecordButton = document.getElementById('create-record-button');
+  let textInputDiv = document.getElementById('text-input-div');
+
+  createRecordButton.disabled = true;
+
+  if (!textInputDiv) {
+    createTextInputDiv();
+    textInputDiv = document.getElementById('text-input-div');
+  } else {
+    textInputDiv.style.display = '';
+  }
 }
