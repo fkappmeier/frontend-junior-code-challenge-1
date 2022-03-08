baa4f490819c784b5282397e332b634514b60416
Fredrik Kappmeier <fredrik.kappmeier@live.de>
2022-03-08 18:42:20 +0100

    Cells in the table can now be clicked to edit the text content - changes to data still need to be applied to the stored data buffer

--- src/js/tableCreator.js
+++ src/js/tableCreator.js
@@ -1,5 +1,62 @@
 import '../styles/table.less';
 
+// Change new string in data buffer
+function saveCellEdit(newString, cell) {
+  const cellBuffer = cell;
+  cellBuffer.innerHTML = newString;
+  cellBuffer.editMode = false;
+
+  // TODO: Save changes to data buffer
+}
+
+// Behavior of cell when clicked on
+function tableCellOnClick(clickedCell) {
+  const cell = clickedCell;
+
+  if (cell.editMode) {
+    return;
+  }
+
+  cell.editMode = true;
+
+  const cellContent = cell.innerHTML;
+  let textInput;
+
+  cell.innerHTML = '';
+
+  if (cellContent.length < 20) {
+    textInput = document.createElement('input');
+  } else {
+    textInput = document.createElement('textarea');
+  }
+
+  textInput.value = cellContent;
+  cell.appendChild(textInput);
+  cell.appendChild(document.createElement('br'));
+
+  const saveButton = document.createElement('button');
+  const cancelButton = document.createElement('button');
+  saveButton.innerHTML = 'Save';
+  cancelButton.innerHTML = 'Cancel';
+
+  saveButton.addEventListener('click', () => {
+    saveButton.disabled = true;
+    cancelButton.disabled = true;
+    saveCellEdit(textInput.value, cell);
+  });
+
+  cancelButton.addEventListener('click', () => {
+    saveButton.disabled = true;
+    cancelButton.disabled = true;
+    cell.innerHTML = cellContent;
+    cell.editMode = false;
+  });
+
+  cell.appendChild(saveButton);
+  cell.appendChild(cancelButton);
+}
+
+// Creates the table from data read from the csv file
 export function createTable(data) {
   const table = document.getElementById('csv-table');
   table.style.display = 'none';
@@ -11,12 +68,24 @@ export function createTable(data) {
       dataRow.forEach((dataCell) => {
         const tableHeader = document.createElement('th');
         tableHeader.innerHTML = dataCell;
+        tableHeader.addEventListener('click', (event) => {
+          // prohibits children of cell from inhereting this event listener
+          if (event.target === event.currentTarget) {
+            tableCellOnClick(event.target);
+          }
+        });
         tableRow.appendChild(tableHeader);
       });
     } else {
       dataRow.forEach((dataCell) => {
         const tableData = document.createElement('td');
         tableData.innerHTML = dataCell;
+        tableData.addEventListener('click', (event) => {
+          // prohibits children of cell from inhereting this event listener
+          if (event.target === event.currentTarget) {
+            tableCellOnClick(event.target);
+          }
+        });
         tableRow.appendChild(tableData);
       });
     }
@@ -28,6 +97,7 @@ export function createTable(data) {
   document.getElementById('create-record-div').style.display = '';
 }
 
+// Adds a table row to the bottom of the table
 export function addTableRow(dataRow) {
   const table = document.getElementById('csv-table');
   const tableRow = document.createElement('tr');
